1fd8bff998c067a8c34b72b56769eb0cbeede582
Sarah <sarahwbcodes@gmail.com>
2018-05-07 22:27:42 +0300

    Added semicolons

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ function(x, y){
     this.speed = (Math.ceil(Math.random()*450)+1);
     //sprite
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x += this.speed*dt
+  this.x += this.speed*dt;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -38,19 +38,19 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 var Player = function(){
-this.sprite = 'images/char-horn-girl.png'
+this.sprite = 'images/char-horn-girl.png';
 //starting coordinates
 this.x=200;
-this.y=400
-}
+this.y=400;
+};
 // This class requires an update(), render() and
 // a handleInput() method.
 //reset
-Player.prototype.reset= function(){
+Player.prototype.reset= function(x,y){
 //back to initial spot or starting coordinates
   this.x=200;
   this.y=400;
-}
+};
 //update
 Player.prototype.update= function(){
   if (this.y<0 || this.x<0){
@@ -65,16 +65,16 @@ Player.prototype.render = function() {
 //handleInput
 Player.prototype.handleInput=function(keyPress){
 if(keyPress=='left' && this.x>0){
-  this.x-=101
+  this.x-=101;
 }
 else if (keyPress=='right' && this.x<505){
-  this.x+=101
+  this.x+=101;
 }
 else if (keyPress=='up' && this.y>0){
-  this.y-=83
+  this.y-=83;
 }
 else if (keyPress=='down' && this.y<505){
-  this.y+=83
+  this.y+=83;
 }
 };
 // Now instantiate your objects.
